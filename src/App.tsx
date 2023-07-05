7d445d6f69d40eb4d61f3f3434537c8f4b982ccd
Masaki Kobayashi <contact@mkobayashi.me>
2023-07-05 20:27:33 +0900

    Basic impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import EmojiPicker, { EmojiStyle, Theme } from "emoji-picker-react";
+import styles from "./App.module.scss";
 
 function App() {
-  return <></>;
+  return (
+    <div className={styles.wrapper}>
+      <EmojiPicker
+        onEmojiClick={(e) => navigator.clipboard.writeText(e.emoji)}
+        theme={Theme.DARK}
+        emojiStyle={EmojiStyle.TWITTER}
+        width={500}
+        height={600}
+      />
+    </div>
+  );
 }
 
 export default App;
